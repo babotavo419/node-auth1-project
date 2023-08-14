014d7b4377048310153b665b007540418c4b16f0
babotavo419 <one.faith419@gmail.com>
2023-08-14 05:35:20 -0400

    ok

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -34,14 +34,14 @@ router.post('/login',
         const { password } = req.body;
 
         // Ensure req.userData is defined and contains the password.
-        if (!req.userData || !req.userData.password) {
+        if (!req.user || !req.user.password) {
             return res.status(500).json({ message: 'Internal server error.' });
         }
 
         // check if the provided password matches the one in the database
-        if (bcrypt.compareSync(password, req.userData.password)) {
-            req.session.user = req.userData;
-            res.status(200).json({ message: `Welcome ${req.userData.username}!` });
+        if (bcrypt.compareSync(password, req.user.password)) {
+            req.session.user = req.user;
+            res.status(200).json({ message: `Welcome ${req.user.username}!` });
         } else {
             res.status(401).json({ message: 'Invalid credentials' });
         }
