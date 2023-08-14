87114e4f06f5daba3ab5c10fd1790b21d6c8a0b0
babotavo419 <one.faith419@gmail.com>
2023-08-14 06:01:35 -0400

    ok

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,33 +1,30 @@
 const Users = require('../users/users-model'); 
 
-// Check for active session
 function restricted(req, res, next) {
-    if (req.session && req.session.user) {
-        next();
-    } else {
-        res.status(401).json({ message: "You shall not pass!" });
-    }
+  if (req.session && req.session.user) {
+      next();
+  } else {
+      res.status(401).json({ message: "You shall not pass!" });
+  }
 }
 
-// Check if username is free
 async function checkUsernameFree(req, res, next) {
-    try {
-        const users = await Users.findBy({ username: req.body.username });
-        if (users.length) {
-            res.status(422).json({ message: "Username taken" });
-        } else {
-            next();
-        }
-    } catch (err) {
-        next(err);
-    }
+  try {
+      const users = await Users.findBy({ username: req.body.username });
+      if (users.length) {
+          res.status(422).json({ message: "Username taken" });
+      } else {
+          next();
+      }
+  } catch (err) {
+      next(err);
+  }
 }
 
-// Check if username exists and has necessary properties
 async function checkUsernameExists(req, res, next) {
   try {
       const users = await Users.findBy({ username: req.body.username });
-      if (!users.length || !users[0].password || !users[0].username) {
+      if (!users.length) {
           res.status(401).json({ message: "Invalid credentials" });
       } else {
           req.user = users[0];
@@ -38,19 +35,20 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-// Check password length
 function checkPasswordLength(req, res, next) {
-    if (!req.body.password || req.body.password.length <= 3) {
-        res.status(422).json({ message: "Password must be longer than 3 chars" });
-    } else {
-        next();
-    }
+  const { password } = req.body;
+
+  if (!password || password.length <= 3) {
+      res.status(422).json({ message: "Password must be longer than 3 chars" });
+  } else {
+      next();
+  }
 }
 
 module.exports = {
-    restricted,
-    checkUsernameFree,
-    checkUsernameExists,
-    checkPasswordLength
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
 };
 
